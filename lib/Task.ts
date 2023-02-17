5d661528566be855ecc814389f1adb99cca7de91
Guohongshen <Guohongshen2018@outlook.com>
2023-02-17 22:04:31 +0800

    docs: 缩进改为四个空格长度

--- lib/Task.ts
+++ lib/Task.ts
@@ -13,41 +13,41 @@ export type Executor = () => Executor;
  * 任务类
  */
 export default class Task {
-  /**
-   * 下一个可分配的任务 id
-   */
-  public static nextId = 1;
-  /**
-   * 任务 id，唯一标识
-   */
-  public id: number;
-  /**
-   * 任务执行函数
-   */
-  public executor: Executor;
-  /**
-   * 任务优先级
-   */
-  public priority: Priority;
-  /**
-   * 在这一刻以及之后，任务可以被执行
-   */
-  public startTime: number;
-  /**
-   * 在这一刻以及之前，任务必须执行完
-   */
-  public deadline: number;
-  constructor(currentTime: number, priority: Priority, executor: Executor, options?: { delay: number }) {
-    currentTime = currentTime || getCurrentTime(); // 以外部传入的时间外准以防止轻微的时间误差
-    let startTime = currentTime;
-    if (options && options.delay && options.delay > 0) {
-      startTime += options.delay;
-    }
-    const deadline = Timeouts[priority] + startTime;
-    this.id = Task.nextId++;
-    this.executor = executor;
-    this.priority = priority;
-    this.startTime = startTime;
-    this.deadline = deadline;
-  }
+	/**
+	 * 下一个可分配的任务 id
+	 */
+	public static nextId = 1;
+	/**
+	 * 任务 id，唯一标识
+	 */
+	public id: number;
+	/**
+	 * 任务执行函数
+	 */
+	public executor: Executor;
+	/**
+	 * 任务优先级
+	 */
+	public priority: Priority;
+	/**
+	 * 在这一刻以及之后，任务可以被执行
+	 */
+	public startTime: number;
+	/**
+	 * 在这一刻以及之前，任务必须执行完
+	 */
+	public deadline: number;
+	constructor(currentTime: number, priority: Priority, executor: Executor, options?: { delay: number }) {
+		currentTime = currentTime || getCurrentTime(); // 以外部传入的时间外准以防止轻微的时间误差
+		let startTime = currentTime;
+		if (options && options.delay && options.delay > 0) {
+			startTime += options.delay;
+		}
+		const deadline = Timeouts[priority] + startTime;
+		this.id = Task.nextId++;
+		this.executor = executor;
+		this.priority = priority;
+		this.startTime = startTime;
+		this.deadline = deadline;
+	}
 }
