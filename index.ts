e1d02ed13201eb614e40f4ee70450ea1c74ea6ed
guohongshen <guohongshen2018@utlook.com>
2024-11-11 15:19:47 +0800

    fix: 修复 bug、优化代码等

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { Priority } from "./lib/Priority";
+import Scheduler from "./lib/Scheduler";
+
+const scheduler = new Scheduler();
+
+export { Priority, Scheduler };
+
+export default scheduler;
--- lib/Priority.ts
+++ lib/Priority.ts
@@ -1,23 +1,27 @@
 /**
- * 调度器的优先级
+ * 任务的优先级
  */
-
-export type Priority = number;
-
-export const Priorities = {
-	NoPriority: 0,
-	ImmediatePriority: 1,
-	UserBlockingPriority: 2,
-	NormalPriority: 3,
-	LowPriority: 4,
-	IdlePriority: 5
+export enum Priority {
+	NoPriority = 0,
+	ImmediatePriority = 1,
+	UserBlockingPriority = 2,
+	NormalPriority = 3,
+	LowPriority = 4,
+	IdlePriority = 5
 };
 
+/**
+ * Max 31 bit integer. The max integer size in V8 for 32-bit systems.
+ * Math.pow(2, 30) - 1
+ * 0b111111111111111111111111111111
+ */
+const maxSigned31BitInt = 1073741823;
+
 export const Timeouts = {
-	[Priorities.NoPriority]: 5000,
-	[Priorities.ImmediatePriority]: -1,
-	[Priorities.UserBlockingPriority]: 250,
-	[Priorities.NormalPriority]: 5000,
-	[Priorities.LowPriority]: 10000,
-	[Priorities.IdlePriority]: 99999999 // 先设置一个非常大的值
+	[Priority.NoPriority]: 5000,
+	[Priority.ImmediatePriority]: -1,
+	[Priority.UserBlockingPriority]: 250,
+	[Priority.NormalPriority]: 5000,
+	[Priority.LowPriority]: 10000,
+	[Priority.IdlePriority]: maxSigned31BitInt // 先设置一个非常大的值
 }
